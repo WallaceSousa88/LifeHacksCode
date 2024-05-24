2e2a4748727db82cfe0493050cef8a8b6831e075
WallaceSousa88 <wallaceinaciogs@gmail.com>
2024-05-24 18:12:29 -0300

    upd auto_click

--- Python/auto_click/auto_click.py
+++ Python/auto_click/auto_click.py
@@ -2,28 +2,26 @@
 # pip install pynput
 # pip install keyboard
 
-from pynput.mouse import Button, Controller
+from pynput.mouse import Button as MouseButton, Controller
 import time
 import keyboard
 import threading
+from tkinter import *
 
 mouse = Controller()
 auto_clicker_ativo = False
+thread_auto_clicker = None
 
-def auto_clicker():
+def auto_clicker(botao, tempo_entre_cliques):
     global auto_clicker_ativo
-    botao = input("Qual botão do mouse? (L / R): ").lower()
-    tempo_entre_cliques = float(input("Intervalo entre cliques (em segundos): "))
 
     if botao == "l":
-        botao_mouse = Button.left
+        botao_mouse = MouseButton.left
     elif botao == "r":
-        botao_mouse = Button.right
+        botao_mouse = MouseButton.right
     else:
         print("Botão inválido.")
         return
-
-    print(f"Auto Clicker pronto para ser ativado com a tecla 'F4'.")
 
     def on_f4_press(event):
         global auto_clicker_ativo
@@ -45,6 +43,27 @@ def auto_clicker():
         else:
             time.sleep(0.1)
 
-threading.Thread(target=auto_clicker).start()
+def iniciar_auto_clicker():
+    global thread_auto_clicker
+    botao = var.get()
+    tempo_entre_cliques = float(entry.get())
+    thread_auto_clicker = threading.Thread(target=auto_clicker, args=(botao, tempo_entre_cliques))
+    thread_auto_clicker.start()
 
-keyboard.wait()
+root = Tk()
+
+var = StringVar()
+R1 = Radiobutton(root, text="Botão esquerdo", variable=var, value='l')
+R1.pack( anchor = W )
+
+R2 = Radiobutton(root, text="Botão direito", variable=var, value='r')
+R2.pack( anchor = W )
+
+Label(root, text="Intervalo entre cliques (em segundos):").pack()
+entry = Entry(root)
+entry.pack()
+
+Button(root, text="Aplicar", command=iniciar_auto_clicker).pack()
+
+root.protocol("WM_DELETE_WINDOW", root.quit)
+root.mainloop()
