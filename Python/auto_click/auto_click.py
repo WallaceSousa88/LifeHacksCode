fdba99721b78409c87fc1114c2a648eb67f42793
WallaceSousa88 <wallaceinaciogs@gmail.com>
2024-05-28 21:23:26 -0300

    upd auto_click

--- Python/auto_click/auto_click.py
+++ Python/auto_click/auto_click.py
@@ -2,83 +2,62 @@
 # pip install pynput
 # pip install keyboard
 
-import sys
-from pynput.mouse import Button as MouseButton, Controller
-import time
+import pyautogui
 import keyboard
-import threading
-from tkinter import *
 import random
+import time
+import threading
 
-mouse = Controller()
-auto_clicker_ativo = False
-thread_auto_clicker = None
+class Listener:
+    def __init__(self):
+        self.__listKey = []
+        self.__hotkey = {}
+        self.__validKey = []
 
-def auto_clicker(botao, tempo_entre_cliques):
-    global auto_clicker_ativo
+    def start(self):
+        while True:
+            key = keyboard.read_event()
+            if key.name in self.__validKey and key.event_type == 'down':
+                if key.name not in self.__listKey:
+                    self.__listKey.append(key.name)
+                _hotkey = '+'.join(self.__listKey)
+                if _hotkey in self.__hotkey:
+                    func, args = self.__hotkey[_hotkey]
+                    func(*args)
+                else:
+                    continue
+            else:
+                self.__listKey.clear()
+                continue
 
-    if botao == "l":
-        botao_mouse = MouseButton.left
-    elif botao == "r":
-        botao_mouse = MouseButton.right
+    def addHotKey(self, hotkey, callback, *args):
+        self.__hotkey[hotkey] = callback, args
+        for _hotkey in hotkey.split('+'):
+            self.__validKey.append(_hotkey)
+
+loop_active = False
+
+def press_key_randomly():
+    keys = ['num1', 'num2', 'num3', 'num4']
+    while loop_active:
+        key_to_press = random.choice(keys)
+        pyautogui.press(key_to_press)
+        time_interval = random.uniform(0.1, 1)
+        time.sleep(time_interval)
+
+def toggle_loop():
+    global loop_active
+    loop_active = not loop_active
+    if loop_active:
+        print("Loop ativado.")
+        threading.Thread(target=press_key_randomly).start()
     else:
-        print("Botão inválido.")
-        return
+        print("Loop desativado.")
 
-    def on_f4_press(event):
-        global auto_clicker_ativo
-        if event.event_type == 'down':
-            return
-        auto_clicker_ativo = not auto_clicker_ativo
-        if auto_clicker_ativo:
-            print("Auto Clicker ativado.")
-        else:
-            print("Auto Clicker desativado.")
+def main():
+    lis = Listener()
+    lis.addHotKey('f4', toggle_loop)
+    lis.start()
 
-    keyboard.hook_key('f4', on_f4_press)
-
-    while True:
-        if auto_clicker_ativo:
-            mouse.press(botao_mouse)
-            mouse.release(botao_mouse)
-            keyboard.press_and_release(str(random.randint(1, 4)))
-            for _ in range(int(tempo_entre_cliques * 10)):
-                if not auto_clicker_ativo:
-                    break
-                time.sleep(random.randint(100, 1000) / 1000)
-        else:
-            time.sleep(0.1)
-
-def iniciar_auto_clicker():
-    global thread_auto_clicker
-    botao = var.get()
-    tempo_entre_cliques = float(entry.get())
-    thread_auto_clicker = threading.Thread(target=auto_clicker, args=(botao, tempo_entre_cliques))
-    thread_auto_clicker.start()
-
-def fechar_programa():
-    global auto_clicker_ativo
-    global thread_auto_clicker
-    auto_clicker_ativo = False
-    if thread_auto_clicker is not None:
-        thread_auto_clicker.join()
-    root.quit()
-    sys.exit()
-
-root = Tk()
-
-var = StringVar()
-R1 = Radiobutton(root, text="Botão esquerdo", variable=var, value='l')
-R1.pack( anchor = W )
-
-R2 = Radiobutton(root, text="Botão direito", variable=var, value='r')
-R2.pack( anchor = W )
-
-Label(root, text="Intervalo entre cliques (em segundos):").pack()
-entry = Entry(root)
-entry.pack()
-
-Button(root, text="Aplicar", command=iniciar_auto_clicker).pack()
-
-root.protocol("WM_DELETE_WINDOW", fechar_programa)
-root.mainloop()
+print("Pressione F4 para iniciar ou parar o programa.")
+main()
