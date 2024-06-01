e6f059054577ea51159331cc86b35a381a50b4a9
WallaceSousa88 <wallaceinaciogs@gmail.com>
2024-06-01 08:41:55 -0300

    upd auto_click

--- Python/auto_click/auto_click.py
+++ Python/auto_click/auto_click.py
@@ -7,6 +7,9 @@ import keyboard
 import random
 import time
 import threading
+import logging
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 class Listener:
     def __init__(self):
@@ -16,19 +19,22 @@ class Listener:
 
     def start(self):
         while True:
-            key = keyboard.read_event()
-            if key.name in self.__validKey and key.event_type == 'down':
-                if key.name not in self.__listKey:
-                    self.__listKey.append(key.name)
-                _hotkey = '+'.join(self.__listKey)
-                if _hotkey in self.__hotkey:
-                    func, args = self.__hotkey[_hotkey]
-                    func(*args)
+            try:
+                key = keyboard.read_event()
+                if key.name in self.__validKey and key.event_type == 'down':
+                    if key.name not in self.__listKey:
+                        self.__listKey.append(key.name)
+                    _hotkey = '+'.join(self.__listKey)
+                    if _hotkey in self.__hotkey:
+                        func, args = self.__hotkey[_hotkey]
+                        func(*args)
+                    else:
+                        continue
                 else:
+                    self.__listKey.clear()
                     continue
-            else:
-                self.__listKey.clear()
-                continue
+            except Exception as e:
+                logging.error("Erro ocorrido", exc_info=True)
 
     def addHotKey(self, hotkey, callback, *args):
         self.__hotkey[hotkey] = callback, args
@@ -40,10 +46,13 @@ loop_active = False
 def press_key_randomly():
     keys = ['num1', 'num2', 'num3', 'num4']
     while loop_active:
-        key_to_press = random.choice(keys)
-        pyautogui.press(key_to_press)
-        time_interval = random.uniform(0.1, 1)
-        time.sleep(time_interval)
+        try:
+            key_to_press = random.choice(keys)
+            pyautogui.press(key_to_press)
+            time_interval = random.uniform(0.1, 1)
+            time.sleep(time_interval)
+        except Exception as e:
+            logging.error("Erro ocorrido", exc_info=True)
 
 def toggle_loop():
     global loop_active
