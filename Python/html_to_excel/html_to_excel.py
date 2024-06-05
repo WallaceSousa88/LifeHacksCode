eeec5727beb63ae55925faf690c63df8f653c6ee
WallaceSousa88 <wallaceinaciogs@gmail.com>
2024-06-04 21:45:56 -0300

    upd html_to_excel

--- Python/html_to_excel/html_to_excel.py
+++ Python/html_to_excel/html_to_excel.py
@@ -1,39 +1,54 @@
 # pip install beautifulsoup4
 # pip install pandas
 # pip install openpyxl
-
 # pip install beautifulsoup4 pandas openpyxl
 
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
-html_file_path = input('Digite o caminho do arquivo HTML: ')
+def ler_html(caminho):
+    try:
+        with open(caminho, 'r', encoding='utf-8') as arquivo:
+            conteudo_html = arquivo.read()
+        return conteudo_html
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        return None
 
-with open(html_file_path, 'r', encoding='utf-8') as file:
-    html_content = file.read()
+def extrair_dados(conteudo_html):
+    sopa = BeautifulSoup(conteudo_html, 'html.parser')
+    divs_atualizacao = sopa.find_all('div', style='text-align:left;')
 
-soup = BeautifulSoup(html_content, 'html.parser')
+    lista_nomes_maquinas = []
+    lista_ultimas_atualizacoes = []
 
-last_update_divs = soup.find_all('div', style='text-align:left;')
+    for div_atualizacao in divs_atualizacao:
+        nome_maquina = div_atualizacao.find_previous('h2', class_='fs-4 fw-bold').get_text(strip=True)
+        tag_ultima_atualizacao = div_atualizacao.find('p')
+        
+        if tag_ultima_atualizacao:
+            ultima_atualizacao = tag_ultima_atualizacao.get_text(strip=True)
+            ultima_atualizacao = re.sub(r'\.\d+', '', ultima_atualizacao)
+            lista_nomes_maquinas.append(nome_maquina)
+            lista_ultimas_atualizacoes.append(ultima_atualizacao)
 
-machine_names_list = []
-last_updates_list = []
+    return lista_nomes_maquinas, lista_ultimas_atualizacoes
 
-for update_div in last_update_divs:
-    machine_name = update_div.find_previous('h2', class_='fs-4 fw-bold').get_text(strip=True)
-    last_update_tag = update_div.find('p')
-    
-    if last_update_tag:
-        last_update = last_update_tag.get_text(strip=True)
-        last_update = re.sub(r'\.\d+', '', last_update)
-        machine_names_list.append(machine_name)
-        last_updates_list.append(last_update)
+def salvar_excel(nomes_maquinas, ultimas_atualizacoes, caminho_saida):
+    dados = {'Nome da Máquina': nomes_maquinas, 'Última Atualização': ultimas_atualizacoes}
+    df = pd.DataFrame(dados)
+    df.to_excel(caminho_saida, index=False)
 
-data = {'Nome da Máquina': machine_names_list, 'Última Atualização': last_updates_list}
-df = pd.DataFrame(data)
+def main():
+    caminho_html = input('Digite o caminho do arquivo HTML: ')
+    conteudo_html = ler_html(caminho_html)
 
-output_excel_path = input('Digite o local de saída para o arquivo Excel: ')
-df.to_excel(output_excel_path, index=False)
+    if conteudo_html:
+        nomes_maquinas, ultimas_atualizacoes = extrair_dados(conteudo_html)
+        caminho_saida = input('Digite o local de saída para o arquivo Excel: ')
+        salvar_excel(nomes_maquinas, ultimas_atualizacoes, caminho_saida)
+        print(f"Os resultados foram salvos em '{caminho_saida}'.")
 
-print(f"Os resultados foram salvos em '{output_excel_path}'.")
+if __name__ == "__main__":
+    main()
