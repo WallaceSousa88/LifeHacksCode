defa2694539563d83308a64041dd8b4f0c4ccfae
WallaceSousa88 <wallaceinaciogs@gmail.com>
2024-06-13 18:22:09 -0300

    upd map_diretorio

--- Python/map_diretorio/map_diretorio.py
+++ Python/map_diretorio/map_diretorio.py
@@ -1,21 +1,18 @@
 import os
-import sys
 
 def listar_arquivos(caminho, nivel=0):
     for item in os.listdir(caminho):
         espacos = ' ' * nivel
         if os.path.isfile(os.path.join(caminho, item)):
-            print(f'{espacos}- {item}')
+            yield f'{espacos}- {item}\n'
         else:
-            print(f'{espacos}+ {item}')
-            listar_arquivos(os.path.join(caminho, item), nivel + 2)
+            yield f'{espacos}+ {item}\n'
+            yield from listar_arquivos(os.path.join(caminho, item), nivel + 2)
 
 def salvar_em_txt(caminho, arquivo_saida):
-    original_stdout = sys.stdout
     with open(arquivo_saida, 'w') as f:
-        sys.stdout = f
-        listar_arquivos(caminho)
-        sys.stdout = original_stdout
+        for linha in listar_arquivos(caminho):
+            f.write(linha)
 
 caminho = input("Digite o caminho do diretório que você deseja mapear: ")
 arquivo_saida = input("Digite o caminho do arquivo onde você deseja salvar a lista de arquivos: ")
