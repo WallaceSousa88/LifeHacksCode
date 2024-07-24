a02918a9c7aaade81e4ab223100fc3eaaf08de0a
WALLACE INACIO GOMES DE SOUSA <wallace.inacio@cemig.com.br>
2024-07-24 09:29:33 -0300

    upd map_diretorio

--- Python/map_diretorio/map_diretorio.py
+++ Python/map_diretorio/map_diretorio.py
@@ -3,18 +3,21 @@ import os
 def listar_arquivos(caminho, nivel=0):
     for item in os.listdir(caminho):
         espacos = ' ' * nivel
-        if os.path.isfile(os.path.join(caminho, item)):
+        item_caminho = os.path.join(caminho, item)
+        if os.path.isfile(item_caminho):
             yield f'{espacos}- {item}\n'
         else:
             yield f'{espacos}+ {item}\n'
-            yield from listar_arquivos(os.path.join(caminho, item), nivel + 2)
+            yield from listar_arquivos(item_caminho, nivel + 2)
 
-def salvar_em_txt(caminho, arquivo_saida):
-    with open(arquivo_saida, 'w') as f:
+def salvar_em_txt(caminho):
+    nome_arquivo = os.path.basename(caminho) + '.txt'
+    caminho_area_trabalho = os.path.join(os.path.expanduser("~"), "Desktop", nome_arquivo)  
+    with open(caminho_area_trabalho, 'w') as f:
         for linha in listar_arquivos(caminho):
             f.write(linha)
 
 caminho = input("Digite o caminho do diretório que você deseja mapear: ")
-arquivo_saida = input("Digite o caminho do arquivo onde você deseja salvar a lista de arquivos: ")
+salvar_em_txt(caminho)
 
-salvar_em_txt(caminho, arquivo_saida)
+print(f"Lista de arquivos salva em: {os.path.join(os.path.expanduser('~'), 'Desktop')}")
