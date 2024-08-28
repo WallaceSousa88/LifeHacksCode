7653a0481819a0d929a2d83ffb866d45234b124d
WALLACE INACIO GOMES DE SOUSA <wallace.inacio@cemig.com.br>
2024-08-28 09:39:37 -0300

    upd map_diretorio

--- Python/map_diretorio/map_diretorio_simple.py
+++ Python/map_diretorio/map_diretorio_simple.py
@@ -1,13 +1,10 @@
 import os
 
-def listar_arquivos(caminho, ignorar_pastas=None):
-    if ignorar_pastas is None:
-        ignorar_pastas = []
+def listar_arquivos(caminho):
     for root, dirs, files in os.walk(caminho):
-        dirs[:] = [d for d in dirs if d not in ignorar_pastas]
         for file in files:
             relpath = os.path.relpath(os.path.join(root, file), caminho)
-            if os.path.dirname(relpath):  # Verifica se o arquivo está em um subdiretório
+            if os.path.dirname(relpath):
                 yield f"\\{relpath}"
             else:
                 yield relpath
@@ -17,6 +14,7 @@ def salvar_em_txt(caminho):
     caminho_area_trabalho = os.path.join(os.path.expanduser("~"), "Desktop", nome_arquivo)  
     try:
         with open(caminho_area_trabalho, 'w', encoding='utf-8') as f:
+            f.write(f"O diretório base é '{caminho}'. Abaixo está a lista de arquivos e pastas a partir desse diretório:\n\n")
             for linha in listar_arquivos(caminho):
                 f.write(linha + '\n')
         print(f"Lista de arquivos salva em: {caminho_area_trabalho}")
