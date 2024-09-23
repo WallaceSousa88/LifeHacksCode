0efde2148a64a1acaf043047af066e1f105330f9
WALLACE INACIO GOMES DE SOUSA <wallace.inacio@cemig.com.br>
2024-09-23 07:55:37 -0300

    upd smax_tools

--- Python/smax_tools/offering_list/main.py
+++ Python/smax_tools/offering_list/main.py
@@ -50,13 +50,46 @@ for cookie in cookies:
 xsrf_token = selenium_cookies['XSRF-TOKEN']
 smax_auth_token = selenium_cookies['SMAX_AUTH_TOKEN']
 
-target_url = f""
+target_url = f"/rest/{tenant_id}/ems/Offering?layout=Id,DisplayLabel,OfferingType,Service,Status,Service.DisplayLabel,Service.IsDeleted&meta=totalCount&order=Id+asc&size=250&skip=0"
 
 headers = {
-    "" : ""
+    "accept": "application/json, text/plain, */*",
+    "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Microsoft Edge\";v=\"128\"",
+    "x-xsrf-token": xsrf_token,
+    "sec-ch-ua-mobile": "?0",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+    "x-client-tenant-version": "v27",
+    "x-requested-with": "XMLHttpRequest",
+    "x-request-land": "Unknown",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-site": "same-origin",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-dest": "empty",
+    "referer": f"{login_url}saw/Offerings",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "priority": "u=1, i"
 }
 
 response = requests.get(f"{login_url}{target_url}",
                          headers=headers, cookies=selenium_cookies)
+
+if response.status_code == 200:
+    data = response.json()
+
+    parsed_url = urlparse(target_url)
+    path_parts = parsed_url.path.split("/")[1:-1]
+    directory = os.path.join(os.getcwd(), *path_parts)
+    os.makedirs(directory, exist_ok=True)
+
+    filename = os.path.join(directory, parsed_url.path.split("/")[-1] + ".json")
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data['entities'], f, ensure_ascii=False, indent=4)
+
+    print(f"Dados salvos em: {filename}")
+
+else:
+    print(f"Erro na requisição: {response.status_code}")
 
 driver(quit)
