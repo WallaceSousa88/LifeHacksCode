a657557bbfd0a2fe1dc2d6f23bfd3bc21b798647
WallaceSousa88 <wallaceinaciogs@gmail.com>
2025-01-26 21:04:02 -0300

    upd xlsx_to_json

---
+++ Python/xlsx_to_json/v2/functions/extract_pdf.py
@@ -0,0 +1,28 @@
+import os
+import pdfplumber
+
+def extract_pdf(pasta_entrada, pasta_saida):
+    def extract_data(caminho_pdf):
+        with pdfplumber.open(caminho_pdf) as pdf:
+            texto_formatado = ""
+            for pagina in pdf.pages:
+                tabelas = pagina.extract_tables()
+                for tabela in tabelas:
+                    for linha in tabela:
+                        linha = [str(item) if item is not None else '' for item in linha]
+                        linha_formatada = ";".join(linha)
+                        texto_formatado += linha_formatada + "\n"
+        return texto_formatado
+
+    if not os.path.exists(pasta_saida):
+        os.makedirs(pasta_saida)
+
+    for nome_arquivo in os.listdir(pasta_entrada):
+        if nome_arquivo.endswith(".pdf"):
+            caminho_pdf = os.path.join(pasta_entrada, nome_arquivo)
+            dados_extraidos = extract_data(caminho_pdf)
+
+            caminho_saida = os.path.join(pasta_saida, nome_arquivo.replace(".pdf", ".csv"))
+
+            with open(caminho_saida, 'w', encoding='utf-8') as f:
+                f.write(dados_extraidos)
