01499ed7509afd3d3d5036f77a71b5cac7af16a9
WallaceSousa88 <wallaceinaciogs@gmail.com>
2025-02-12 17:04:48 -0300

    upd map_diretorio

--- Python/map_diretorio/main.py
+++ Python/map_diretorio/main.py
@@ -10,22 +10,23 @@ def salvar_em_txt(caminho):
     caminho_area_trabalho = os.path.join(os.path.expanduser("~"), "Desktop", nome_arquivo)
     try:
         with open(caminho_area_trabalho, 'w', encoding='utf-8') as f:
-            f.write(f"O diretório base é '{caminho}'. Abaixo está a lista de arquivos e seus conteúdos a partir desse diretório:\n\n")
+            f.write("Abaixo está a lista dos arquivos neste diretório, seguida pelos conteúdos de cada arquivo:\n\n")
             for arquivo in listar_arquivos(caminho):
-                f.write(f"Endereço completo do arquivo: {arquivo}\n")
+                nome_do_arquivo = os.path.basename(arquivo)
+                f.write(f"Arquivo: {nome_do_arquivo} tem o codigo:\n")
                 try:
                     with open(arquivo, 'r', encoding='utf-8') as file_content:
                         f.write(file_content.read() + '\n')
                 except Exception as e:
                     f.write(f"Erro ao ler o arquivo: {e}\n")
-        print(f"Lista de arquivos salva em: {caminho_area_trabalho}")
+        print(f"Lista de arquivos e seus conteúdos salva em: {caminho_area_trabalho}")
     except PermissionError:
         print(f"Erro: Permissão negada para escrever em: {caminho_area_trabalho}")
     except Exception as e:
         print(f"Erro ao salvar o arquivo: {e}")
 
 def main():
-    caminho = input("Digite o caminho do diretório que você deseja mapear: ")
+    caminho = input("Digite o caminho do diretório para mapear: ")
     if os.path.isdir(caminho):
         salvar_em_txt(caminho)
     else:
