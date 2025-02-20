ab31656abc348242abc093f81cff6c63668107a4
WallaceSousa88 <wallaceinaciogs@gmail.com>
2025-02-19 22:41:18 -0300

    upd xlsx_to_json

--- Python/xlsx_to_json/xlsx_to_mermaid.py
+++ Python/xlsx_to_json/xlsx_to_mermaid.py
@@ -4,13 +4,13 @@ def generate_mermaid_code(file_path, output_path):
 
     lines = [line.strip() for line in lines if line.strip()]
     node_map = {}
-    node_counter = 1
-    mermaid_code = ""
+    node_counter = 0
+    mermaid_code = "%%{init: {'theme': 'default', 'themeVariables': { 'primaryColor': '#ffffff', 'edgeColor': '#000000', 'nodeTextColor': '#000000'}}}%%\n\nflowchart LR\n\n"
 
     def get_node_variable(node_name):
         nonlocal node_counter
         if node_name not in node_map:
-            node_map[node_name] = f"node{node_counter}"
+            node_map[node_name] = f"{node_counter:03}"
             node_counter += 1
         return node_map[node_name]
 
@@ -29,7 +29,7 @@ def generate_mermaid_code(file_path, output_path):
         output_file.write(mermaid_code)
 
 file_path = 'a.txt'
-output_path = 'mermaid_output.txt'
+output_path = 'fluxograma.mmd'
 
 generate_mermaid_code(file_path, output_path)
-print(f"Arquivo de saida: {output_path}")
+print(f"Arquivo de saída: {output_path}")
