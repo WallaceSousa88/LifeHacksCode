4d252976a180c64217839641494332d3402414fb
WallaceSousa88 <wallaceinaciogs@gmail.com>
2025-04-01 22:45:25 -0300

    upd edge_scraper

--- Python/edge_scraper/main.py
+++ Python/edge_scraper/main.py
@@ -6,22 +6,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-def salvar_codigo_fonte(driver: webdriver.Edge, url: str, output_file: str = 'codigo_fonte.html', wait_time: int = 10) -> None:
+
+def criar_driver(headless: bool = True) -> webdriver.Edge:
+    options = webdriver.EdgeOptions()
+    if headless:
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+    service = Service(EdgeChromiumDriverManager().install())
+    driver = webdriver.Edge(service=service, options=options)
+    return driver
+
+
+def salvar_codigo_fonte(driver: webdriver.Edge, url: str,
+                        output_file: str = 'codigo_fonte.html', wait_time: int = 10) -> None:
     try:
+        logging.info(f"Acessando URL: {url}")
         driver.get(url)
-        WebDriverWait(driver, wait_time).until(lambda d: d.execute_script("return document.readyState") == "complete")
+
+        WebDriverWait(driver, wait_time).until(
+            lambda d: d.execute_script("return document.readyState") == "complete"
+        )
+
         codigo_fonte = driver.page_source
+
         with open(output_file, 'w', encoding='utf-8') as file:
             file.write(codigo_fonte)
-        logging.info("Código-fonte salvo!")
+
+        logging.info(f"Código-fonte salvo em: {output_file}")
     except Exception as e:
-        logging.error(f"Ocorreu o erro: {e}")
+        logging.exception(f"Ocorreu um erro ao acessar a URL '{url}'.")
+
 
 def main() -> None:
-    url: str = 'https://example.com'
-    service: Service = Service(EdgeChromiumDriverManager().install())
-    with webdriver.Edge(service=service) as driver:
-        salvar_codigo_fonte(driver, url)
+    url = 'https://example.com'
+    try:
+        with criar_driver(headless=False) as driver:
+            salvar_codigo_fonte(driver, url=url)
+    except Exception as e:
+        logging.exception("Falha ao executar a função main.")
+
 
 if __name__ == '__main__':
     main()
