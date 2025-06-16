67ca51b7a9b8df3754c4541fffd90cb3dfb69b47
WallaceSousa88 <wallaceinaciogs@gmail.com>
2025-06-16 20:40:12 -0300

    upd smax_tools

---
+++ Python/smax_tools/v2/main.py
@@ -0,0 +1,10 @@
+from utils import convert_utf8 , rmv_space , title_case , lowercase_words , get_label , get_rules
+
+file_path = 'x.txt'
+
+convert_utf8(file_path)
+rmv_space(file_path)
+title_case(file_path)
+lowercase_words(file_path)
+get_label(file_path)
+get_rules(file_path)
