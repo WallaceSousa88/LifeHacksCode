c7707c906d8c214a9b52ad935c97ea127b3ca15f
WallaceSousa88 <wallaceinaciogs@gmail.com>
2026-01-13 21:50:53 -0300

    upd sonarqube

--- Python/update_sonarqube/list_pipeline.py
+++ Python/update_sonarqube/list_pipeline.py
@@ -1,30 +1,47 @@
+import os
+import csv
 import requests
 
 from requests.auth import HTTPBasicAuth
 
-token = 'x'
-organization = 'x'
+token = os.getenv("AZURE_TOKEN")
+organization = os.getenv("AZURE_ORG")
 
-url_projects = f'https://dev.azure.com/{organization}/_apis/projects?api-version=6.0'
+session = requests.Session()
+session.auth = HTTPBasicAuth('', token)
 
-response_projects = requests.get(url_projects, auth=HTTPBasicAuth('', token))
+def get_projects():
+    url = f'https://dev.azure.com/{organization}/_apis/projects?api-version=6.0'
+    response = session.get(url)
+    if response.ok:
+        data = response.json()
+        return sorted(data['value'], key=lambda x: x['name'])
+    else:
+        raise Exception(f"Erro {response.status_code}: {response.text}")
 
-if response_projects.status_code == 200:
-    data_projects = response_projects.json()
-    sorted_projects = sorted(data_projects['value'], key=lambda x: x['name'])
+def get_pipelines(project_name):
+    url = f'https://dev.azure.com/{organization}/{project_name}/_apis/pipelines?api-version=6.0'
+    response = session.get(url)
+    if response.ok:
+        return response.json()['value']
+    else:
+        return []
 
+def main():
+    projects = get_projects()
     total_pipelines = 0
-    with open('pipelines.txt', 'w', encoding='utf-8') as file:
-        for project in sorted_projects:
-            project_name = project["name"]
-            url_pipelines = f'https://dev.azure.com/{organization}/{project_name}/_apis/pipelines?api-version=6.0'
-            response_pipelines = requests.get(url_pipelines, auth=HTTPBasicAuth('', token))
-            if response_pipelines.status_code == 200:
-                data_pipelines = response_pipelines.json()
-                num_pipelines = len(data_pipelines['value'])
-                total_pipelines += num_pipelines
-                file.write(f'{project_name}, {num_pipelines}\n')
-        file.write(f'\nTotal de pipelines em todos os projetos: {total_pipelines}\n')
-    print(f'A lista de projetos e o número de pipelines foram exportados para "pipelines.txt". Total de projetos encontrados: {len(sorted_projects)}. Total de pipelines: {total_pipelines}.')
-else:
-    print('Falha na solicitação: ', response_projects.status_code)
+    with open('pipelines.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Projeto", "Número de Pipelines", "Nomes dos Pipelines"])
+        for project in projects:
+            pipelines = get_pipelines(project["name"])
+            num_pipelines = len(pipelines)
+            total_pipelines += num_pipelines
+            pipeline_names = "; ".join([p["name"] for p in pipelines])
+            writer.writerow([project["name"], num_pipelines, pipeline_names])
+        writer.writerow([])
+        writer.writerow(["Total de pipelines", total_pipelines])
+    print(f'Total de projetos: {len(projects)}. Total de pipelines: {total_pipelines}.')
+
+if __name__ == "__main__":
+    main()
